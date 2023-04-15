78833ace93739615a4982dcb7ee9afbd0835f792
Gr34sy <slupek3@poczta.fm>
2023-04-15 13:33:45 +0200

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        black: colors.black,
-        white: colors.white,
-        gray: colors.gray,
-        emerald: colors.emerald,
-        indigo: colors.indigo,
-        yellow: colors.yellow,
+        primary: '#FF6363',
       },
     },
   },
